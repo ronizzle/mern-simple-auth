6b7fdd38fbe9728d9f2d6d58a9fc02f5f41cbd3d
Roniel Bernas <roniel@eligeo.com>
2021-05-21 09:07:06 +0800

    Server  - passport strategy implementation  - server.js serving as the main entry point of the server  - User model  - server endpoints

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const UserSchema = new Schema({
+    first_name: {
+        type: String,
+        required: true
+    },
+    middle_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    contact_number: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date_created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = User = mongoose.model('users', UserSchema);
